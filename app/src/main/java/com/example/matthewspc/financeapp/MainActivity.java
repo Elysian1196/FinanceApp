f70c7a818bf3543b0f212dd6b7b3a4ba46353ddc
greygxz <33404387+greygxz@users.noreply.github.com>
2017-11-05 20:07:21 -0500

    added button methods

--- app/src/main/java/com/example/matthewspc/financeapp/MainActivity.java
+++ app/src/main/java/com/example/matthewspc/financeapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.matthewspc.financeapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void graphButton(View view)
+    {
+        //
+    }
+
+    public void multiButton(View view)
+    {
+        //
+    }
+
+    public void budgetButton(View view)
+    {
+        //
+    }
+
+    public void purchaseButton(View view)
+    {
+        //
     }
 }
