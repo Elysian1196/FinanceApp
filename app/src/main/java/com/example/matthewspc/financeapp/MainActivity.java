acce490c05d3c10a318f4ebe32bd44a7f570b860
Julia Zhu <zzhu52813@gmail.com>
2017-11-28 21:57:45 -0500

    second activity

--- app/src/main/java/com/example/matthewspc/financeapp/MainActivity.java
+++ app/src/main/java/com/example/matthewspc/financeapp/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.matthewspc.financeapp;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -17,15 +27,29 @@ public class MainActivity extends AppCompatActivity
     private TextView budjetResult;
     private TextView budjetLeftResult;
 
+
+    private float[] yData = {25.3f, 42.6f, 66.76f, 44,32f, 46.01f, 48.89f, 23.9f};
+    private String[] xData = {"Groceries", "Dining Out","Rent","Utilities","Travel","Clothes","Other"};
+    PieChart pieChart;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dateResult=(TextView)this.findViewById(R.id.dateResult);
-        spendResult=(TextView)this.findViewById(R.id.spendResult);
         budjetResult=(TextView)this.findViewById(R.id.budgetResult);
         budjetLeftResult=(TextView)this.findViewById(R.id.budgetLeftResult);
+        Log.d("MainActivity", "onCreate: starting to create chart");
+        pieChart = (PieChart) findViewById(R.id.idPieChart);
+
+        pieChart.setDescription("Total budget: "+spendGoal);
+        pieChart.setRotationEnabled(true);
+        pieChart.setHoleRadius(25f);
+        pieChart.setTransparentCircleAlpha(0);
+        pieChart.setDrawEntryLabels(true);
+        pieChart.setEntryLabelTextSize(20);
+
+        addDataSet();
     }
 
     public void updateProfile(View view) //defines listener for the UpdateProfile Activity
@@ -59,5 +83,46 @@ public class MainActivity extends AppCompatActivity
                 //customAdapter.notifyDataSetChanged();//tells adapter to chanage listView
             }
         }
+    }
+
+    private void addDataSet() {
+        Log.d("MainActivity", "addDataSet started");
+        ArrayList<PieEntry> yEntrys = new ArrayList<>();
+        ArrayList<String> xEntrys = new ArrayList<>();
+
+        for(int i = 0; i < yData.length; i++){
+            yEntrys.add(new PieEntry(yData[i] , i));
+        }
+
+        for(int i = 1; i < xData.length; i++){
+            xEntrys.add(xData[i]);
+        }
+
+        //create the data set
+        PieDataSet pieDataSet = new PieDataSet(yEntrys, "Expense Percentages");
+        pieDataSet.setSliceSpace(2);
+        pieDataSet.setValueTextSize(12);
+
+        //add colors to dataset
+        ArrayList<Integer> colors = new ArrayList<>();
+        colors.add(Color.WHITE);
+        colors.add(Color.RED);
+        colors.add(Color.GREEN);
+        colors.add(Color.CYAN);
+        colors.add(Color.YELLOW);
+        colors.add(Color.MAGENTA);
+        colors.add(Color.LTGRAY);
+
+        pieDataSet.setColors(colors);
+
+        //add legend to chart
+        Legend legend = pieChart.getLegend();
+        legend.setForm(Legend.LegendForm.CIRCLE);
+        legend.setPosition(Legend.LegendPosition.LEFT_OF_CHART);
+
+        //create pie data object
+        PieData pieData = new PieData(pieDataSet);
+        pieChart.setData(pieData);
+        pieChart.invalidate();
     }
 }
